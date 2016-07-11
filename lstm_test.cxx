52887e8f94c69de464f89ba4287a16f346a79780
Qiang Kou <qkou@umail.iu.edu>
2016-07-11 13:38:44 -0700

    something wrong in lstm, need more time

--- lstm_test.cxx
+++ lstm_test.cxx
@@ -23,7 +23,7 @@ std::vector<Symbol> lstm(int num_hidden, Symbol indata,
                                     gates, 4);
 
   Symbol in_gate = Activation("t" + std::to_string(seqidx) + "_l" + std::to_string(layeridx) + "_in_gates",
-                              slice_gates[0], "sigmod");
+                              slice_gates[0], "sigmoid");
   Symbol in_transform = Activation("t" + std::to_string(seqidx) + "_l" + std::to_string(layeridx) + "_in_transform",
                                    slice_gates[1], "tanh");
   Symbol forget_gate = Activation("t" + std::to_string(seqidx) + "_l" + std::to_string(layeridx) + "_forget_data",
@@ -87,6 +87,7 @@ Symbol lstm_unroll(int num_lstm_layer, int seq_len, int input_size,
 
     last_hidden.push_back(hidden);
   }
+
   Symbol concat = Concat("", last_hidden, last_hidden.size(), 0);
   Symbol fc = FullyConnected("", concat, cls_weight, cls_bias, num_label);
   Symbol sm = SoftmaxOutput("sm", fc, label);
@@ -108,6 +109,21 @@ Symbol lstm_unroll(int num_lstm_layer, int seq_len, int input_size,
   return Symbol::Group(list_all);
 }
 
+bool endswith(std::string const &fullString, std::string const &ending) {
+  if (fullString.length() >= ending.length()) {
+    return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
+  } else {
+    return false;
+  }
+}
+
+bool is_param_name(const std::string & name) {
+  return endswith(name, "weight") ||
+      endswith(name, "bias") ||
+      endswith(name, "gamma") ||
+      endswith(name, "beta");
+}
+
 int main() {
   int batch_size = 32;
   int seq_len = 32;
@@ -119,8 +135,64 @@ int main() {
   mx_float wd = 0.00001;
   int clip_gradient = 1;
   int update_period = 1;
-  int input_size = 63;
+  int input_size = 65;
   int num_label = input_size;
-  Symbol rnn_sym = lstm_unroll(num_lstm_layer, num_hidden, seq_len, input_size,
-                               num_embed, num_label, 0.0);
+
+  Context ctx_dev(DeviceType::kGPU, 0);
+
+  Symbol rnn_sym = lstm_unroll(num_lstm_layer, seq_len, input_size,
+                               num_hidden, num_embed, num_label, 0.0);
+
+  std::vector<std::string> arg_names = rnn_sym.ListArguments();
+
+  std::map<std::string, std::vector<mx_uint> > input_shapes;
+
+  /*for (size_t i = 0; i < arg_names.size(); i++) {*/
+    //if (endswith(arg_names[i], "init_c") || endswith(arg_names[i], "init_h")) {
+      //std::vector<mx_uint> tmp; tmp.push_back(batch_size); tmp.push_back(num_hidden);
+      //input_shapes[arg_names[i]] = tmp; 
+    //} else if (endswith(arg_names[i], "data")) {
+      //std::vector<mx_uint> tmp; tmp.push_back(batch_size);
+      //input_shapes[arg_names[i]] = tmp;
+    //}
+  //}
+
+  //std::vector<std::vector<mx_uint> > arg_shapes, aux_shapes, out_shapes;
+  /*rnn_sym.InferShape(input_shapes, &arg_shapes, &aux_shapes, &out_shapes);*/
+  /*for (size_t i = 0; i < arg_shapes.size(); i++) {*/
+    //for (size_t j = 0; j < arg_shapes[i].size(); j++) {
+      //std::cout << "arg_shapes " << arg_shapes[i][j] << ", ";
+    //}
+    //std::cout << std::endl;
+  /*}*/
+  //for (size_t i = 0; i < out_shapes.size(); i++) {
+  //for (size_t j = 0; j < out_shapes[i].size(); j++) {
+  //std::cout << out_shapes[i][j] << ", ";
+  //}
+  //std::cout << std::endl;
+  /*}*/
+  /*std::vector<NDArray> arg_arrays;*/
+  //for (size_t i = 0; i < arg_shapes.size(); i++) {
+    //NDArray tmp(Shape(arg_shapes[i]), ctx_dev, false);
+    //tmp = (mx_float)0.0;
+    //arg_arrays.push_back(tmp);
+  /*}*/
+
+  //std::cout << arg_names.size() << " " << arg_shapes.size() << std::endl;
+  /*std::vector<NDArray> args_grad;*/
+  //for (size_t i = 0; i < arg_shapes.size(); i++) {
+    //if (is_param_name(arg_names[i])) {
+      //NDArray tmp(Shape(arg_shapes[i]), ctx_dev, false);
+      //tmp = (mx_float)0.0; 
+      //args_grad.push_back(tmp);
+    //}
+  /*}*/
+  //std::cout << __LINE__ << std::endl;
+  /*std::vector<OpReqType> grad_req_type;*/
+  //for (size_t i = 0; i < args_grad.size(); i++)
+    //grad_req_type.push_back(kAddTo);
+
+  //std::vector<NDArray> aux_states;
+  //Executor * exe = new Executor(rnn_sym, ctx_dev, arg_arrays, args_grad,
+                                /*grad_req_type, aux_states);*/
 }
