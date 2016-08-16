a65fda6819eb2895c75aae7a5b9ee2dcbd49bda9
Qiang Kou <qkou@umail.iu.edu>
2016-08-16 15:07:40 -0700

    R scripts for network definition

---
+++ backends/mxnet/test/gen_r_json.R
@@ -0,0 +1,16 @@
+library(mxnet)
+
+#name = "alexnet"
+#name = "googlenet"
+#name = "inception-bn"
+#name = "lenet"
+#name = "mlp"
+#name = "unet"
+#name = "resnet"
+name = "vgg"
+
+source(paste("symbol_", name, ".R", sep = ''))
+
+network <- get_symbol(10)
+
+cat(network$as.json(), file = paste("symbol_", name, "-R.json", sep = ''), sep = "")
