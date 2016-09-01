d8c9482c4dc4c33429f03e640cc412deaa353df7
Arno Candel <arno.candel@gmail.com>
2016-08-31 17:37:42 -0700

    Create all symbols at once.

--- backends/mxnet/test/gen_py_json.py
+++ backends/mxnet/test/gen_py_json.py
@@ -1,17 +1,9 @@
 import mxnet as mx
 import importlib
 
-#name = "alexnet"
-#name = "googlenet"
-#name = "inception-bn"
-name = "inception-v3"
-#name = "lenet"
-#name = "mlp"
-#name = "resnet"
-#name = "vgg"
-
-net = importlib.import_module("symbol_" + name).get_symbol(10)
-net.save("symbol_" + name + "-py.json")
+for name in ["alexnet", "googlenet", "inception-bn", "inception-v3", "lenet", "mlp", "resnet", "vgg"]:
+    net = importlib.import_module("symbol_" + name).get_symbol(10)
+    net.save("symbol_" + name + "-py.json")
 
 name = "unet"
 net = importlib.import_module("symbol_" + name).get_symbol()
--- backends/mxnet/test/gen_r_json.R
+++ backends/mxnet/test/gen_r_json.R
@@ -1,19 +1,10 @@
 library(mxnet)
 
-#name = "alexnet"
-#name = "googlenet"
-#name = "inception-bn"
-name = "inception-v3"
-#name = "lenet"
-#name = "mlp"
-#name = "resnet"
-#name = "vgg"
-
-source(paste("symbol_", name, ".R", sep = ''))
-
-network <- get_symbol(10)
-
-cat(network$as.json(), file = paste("symbol_", name, "-R.json", sep = ''), sep = "")
+for name in c("alexnet", "googlenet", "inception-bn", "inception-v3", "lenet", "mlp", "resnet", "vgg") {
+    source(paste("symbol_", name, ".R", sep = ''))
+    network <- get_symbol(10)
+    cat(network$as.json(), file = paste("symbol_", name, "-R.json", sep = ''), sep = "")
+}
 
 name = "unet"
 
