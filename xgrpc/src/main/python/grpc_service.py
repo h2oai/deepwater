ab8a5e1b4dd7db5d1f12e814629129ab6f3796d3
Magnus Stensmo <mstensmo@gmail.com>
2016-12-16 16:06:02 -0800

    added in LeNet architecture

--- xgrpc/src/main/python/grpc_service.py
+++ xgrpc/src/main/python/grpc_service.py
@@ -66,6 +66,103 @@ def _mlp(width=28, height=28, channels=1, classes=10):
     # FIXME
     return tf.train.export_meta_graph(saver_def=saver.as_saver_def())
 
+# LeNet
+
+def _lenet(width=28, height=28, channels=1, classes=10, clip_gradient=6, mini_batch_size=32):
+  graph = tf.Graph()
+  with graph.as_default():
+    size = width * height * channels
+    x = tf.placeholder(tf.float32, [None, size], name="x")
+
+    # #---
+    # initialization = tf.truncated_normal([size, classes], mean=0.5, stddev=0.2)
+    # w = tf.Variable(initialization, name="W")
+    # b = tf.Variable(tf.zeros([classes]), name="b")
+    # y = tf.matmul(x, w) + b
+    #
+    # y = tf.nn.relu(y)
+    # #---
+
+    # https://www.tensorflow.org/versions/r0.10/tutorials/mnist/pros/
+
+    def weight_variable(shape):
+      initial = tf.truncated_normal(shape, stddev=0.1)
+      return tf.Variable(initial)
+
+    def bias_variable(shape):
+      initial = tf.constant(0.1, shape=shape)
+      return tf.Variable(initial)
+
+    def conv2d(x, W):
+      return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+    def max_pool_2x2(x):
+      return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                            strides=[1, 2, 2, 1], padding='SAME')
+
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+
+    x_image = tf.reshape(x, [-1,width,height,1])
+
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
+
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
+
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    W_fc2 = weight_variable([1024, classes])
+    b_fc2 = bias_variable([classes])
+
+    y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    y = y_conv
+
+    # labels
+    y_ = tf.placeholder(tf.float32, [None, classes], name="y")
+
+    # accuracy
+    correct_prediction = tf.equal(tf.argmax(y, 1),
+                                  tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    # train
+    cross_entropy = tf.reduce_mean(
+      tf.nn.softmax_cross_entropy_with_logits(y, y_))
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+    tf.add_to_collection("train", train_step)
+    # this is required by the h2o tensorflow backend
+    global_step = tf.Variable(0, name="global_step", trainable=False)
+
+    tf.add_to_collection("logits", y)
+    saver = tf.train.Saver()
+    # init = tf.initialize_all_variables()
+    init = tf.global_variables_initializer()
+    tf.add_to_collection("init", init.name)
+
+    meta = json.dumps({
+      "inputs": {"batch_image_input": x.name, "categorical_labels": y_.name},
+      "outputs": {"categorical_logits": y.name},
+      "metrics": {"accuracy": accuracy.name, "total_loss": cross_entropy.name},
+      "parameters": {"global_step": global_step.name},
+    })
+    tf.add_to_collection("meta", meta)
+
+    # FIXME
+    return tf.train.export_meta_graph(saver_def=saver.as_saver_def())
+
 
 def convert_fetches(params):
   return [p.name for p in params]
@@ -98,6 +195,10 @@ def convert_map(params):
 def createModel(modelName, params):
   if modelName == "mlp":
     tf_model = _mlp(**params)
+    serialized = tf_model.SerializeToString()
+    return serialized
+  elif modelName == "lenet":
+    tf_model = _lenet(**params)
     serialized = tf_model.SerializeToString()
     return serialized
   raise Exception("unsupported model {}".format(modelName))
