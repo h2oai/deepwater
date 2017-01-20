974fb30f1a160cba6b73f908d731b05a00e31dbd
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2017-01-19 17:34:26 -0800

    alexnet converging

--- python/deepwater/models/test_alexnet.py
+++ python/deepwater/models/test_alexnet.py
@@ -10,13 +10,13 @@ from deepwater.models.test_utils import MNIST_must_converge
 class TestVGG(unittest.TestCase):
 
     def test_vgg_must_converge_on_MNIST(self):
-        MNIST_must_converge(alexnet.AlexNet,
-                            optimizers.GradientDescentOptimizer,
-                            batch_size=64,
+        MNIST_must_converge("alexnet", alexnet.AlexNet,
+                            optimizers.MomentumOptimizer,
+                            batch_size=16,
                             epochs=90,
-                            initial_learning_rate=0.1,
-                            summaries=True,
-                            use_debug_session=True)
+                            initial_learning_rate=0.01,
+                            summaries=False,
+                            use_debug_session=False)
 
 
 if __name__ == "__main__":
