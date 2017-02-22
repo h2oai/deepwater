2fcf830bb563a5b403f68b1cf6dedf77f907c0e9
Arno Candel <arno.candel@gmail.com>
2017-02-22 13:20:56 -0800

    WIP towards Xavier initialization

--- python/deepwater/models/nn.py
+++ python/deepwater/models/nn.py
@@ -1,4 +1,3 @@
-import math
 import tensorflow as tf
 
 def batch_norm(x, scope=''):
@@ -7,27 +6,7 @@ def batch_norm(x, scope=''):
     return layers_batch_norm(x, epsilon=batch_norm_epsilon)
 
 def weight_variable(shape, name):
-    # Delving deep into Rectifier
-    # http://arxiv.org/pdf/1502.01852v1.pdf)
-    # fan_in, _ = get_fans(shape)
-    if shape:
-        fan_in = float(shape[-2]) if len(shape) > 1 else float(shape[-1])
-        fan_out = float(shape[-1])
-    else:
-        fan_in = 1.0
-        fan_out = 1.0
-    for dim in shape[:-2]:
-        fan_in *= float(dim)
-        fan_out *= float(dim)
-
-    # assert stddev > 0.001, stddev
-
-    factor = 2.0
-    stddev = math.sqrt(1.3 * factor / fan_in)
-
-    initialization = tf.truncated_normal(shape, stddev=stddev)
-
-    return tf.Variable(initialization, name=name)
+    return tf.get_variable(name=name, shape=shape, initializer=tf.contrib.layers.xavier_initializer())
 
 def bias_variable(shape, name):
     initial = tf.constant(0.01, shape=shape)
