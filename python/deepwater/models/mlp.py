a4b39c8508a242d932b8e3e10f2ef0b1a6df4fbd
Magnus Stensmo <mstensmo@gmail.com>
2017-03-07 18:49:00 -0800

    fix dropout in mlp

--- python/deepwater/models/mlp.py
+++ python/deepwater/models/mlp.py
@@ -8,7 +8,7 @@ from deepwater.models.nn import fc
 
 class MultiLayerPerceptron(BaseImageClassificationModel):
     def __init__(self, width=28, height=28, channels=1, classes=10,
-                 hidden_layers=[]):
+                 hidden_layers=[], dropout=[]):
         super(MultiLayerPerceptron, self).__init__()
 
         self._number_of_classes = classes
