fdf442e987fb3ddd790a444c7912ab0b171af4a9
Arno Candel <arno.candel@gmail.com>
2017-03-16 20:34:21 -0700

    Only test Inception V3.

--- python/deepwater/models/test_inception.py
+++ python/deepwater/models/test_inception.py
@@ -9,26 +9,6 @@ from deepwater.models.test_utils import cat_dog_mouse_must_converge
 
 class TestInceptionV3(unittest.TestCase):
 
-    def test_inceptionV3_must_converge_on_MNIST(self):
-        MNIST_must_converge("inceptionV3", inception.InceptionV3,
-                            optimizers.MomentumOptimizer,
-                            batch_size=32,
-                            epochs=90,
-                            initial_learning_rate=1e-3,
-                            summaries=False,
-                            use_debug_session=False,
-                            )
-
-#    def test_inceptionV3_must_converge_on_CIFAR10(self):
-#        CIFAR10_must_converge("inceptionV3", inception.InceptionV3,
-#                              optimizers.MomentumOptimizer,
-#                              batch_size=16,
-#                              epochs=90,
-#                              initial_learning_rate=0.2,
-#                              summaries=False,
-#                              use_debug_session=False,
-#                              )
-
     def test_inceptionV3_cat_dog_mouse_must_converge(self):
         train_error = cat_dog_mouse_must_converge("inceptionV3", inception.InceptionV3,
                                                   optimizers.MomentumOptimizer,
@@ -36,15 +16,6 @@ class TestInceptionV3(unittest.TestCase):
                                                   epochs=500,
                                                   initial_learning_rate=1e-3,
                                                   summaries=False)
-        self.assertTrue(train_error <= 0.1)
-
-    def test_inceptionV4_cat_dog_mouse_must_converge(self):
-        train_error = cat_dog_mouse_must_converge("inceptionV4", inception.InceptionV4,
-                                                      optimizers.MomentumOptimizer,
-                                                      batch_size=32,
-                                                      epochs=500,
-                                                      initial_learning_rate=1e-3,
-                                                      summaries=False)
         self.assertTrue(train_error <= 0.1)
 
 if __name__ == "__main__":
