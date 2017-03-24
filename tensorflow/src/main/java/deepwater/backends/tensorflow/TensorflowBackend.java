d6c3bd419443a48187667a389b2f3e8c035b50d2
mdymczyk <dymczyk@gmail.com>
2017-03-24 13:50:47 +0900

    * Makes H2O specific meta parameters optional * Close the tensors after they are used

--- tensorflow/src/main/java/deepwater/backends/tensorflow/TensorflowBackend.java
+++ tensorflow/src/main/java/deepwater/backends/tensorflow/TensorflowBackend.java
@@ -87,14 +87,22 @@ public class TensorflowBackend implements BackendTrain {
 
         if (!model.meta.init.isEmpty()) {
             Session.Runner runner = session.runner();
-            runner.feed(normalize(model.meta.parameters.get("global_is_training")), Tensor.create(false));
+            Tensor isTrainingTensor = Tensor.create(false);
+            feedIfPresent(runner, model.meta.parameters.get("global_is_training"), isTrainingTensor);
             runner.addTarget(model.meta.init).run();
+            isTrainingTensor.close();
         } else {
             System.out.println("ERROR: no init operation found");
             return null;
         }
         model.setSession(this.session);
         return model;
+    }
+
+    private void feedIfPresent(Session.Runner runner, String value, Tensor tensor) {
+        if(null != value) {
+            runner.feed(normalize(value), tensor);
+        }
     }
 
     @Override
@@ -113,10 +121,14 @@ public class TensorflowBackend implements BackendTrain {
 
         Session.Runner runner = model.getSession().runner();
 
-        runner.feed(normalize(model.meta.save_filename), Tensor.create(param_path.getBytes()));
-        runner.feed(normalize(model.meta.parameters.get("global_is_training")), Tensor.create(false));
-        runner.addTarget(normalize(model.meta.restore_op));
-        runner.run();
+        try(Tensor filenameTensor = Tensor.create(param_path.getBytes());
+            Tensor isTrainingTensor = Tensor.create(false)){
+
+            feedIfPresent(runner, model.meta.save_filename, filenameTensor);
+            feedIfPresent(runner, model.meta.parameters.get("global_is_training"), isTrainingTensor);
+            runner.addTarget(normalize(model.meta.restore_op));
+            runner.run();
+        }
     }
 
     public void writeBytes(File file, byte[] payload) throws IOException {
@@ -154,11 +166,15 @@ public class TensorflowBackend implements BackendTrain {
         TensorflowModel model = (TensorflowModel) m;
         Session.Runner runner = model.getSession().runner();
 
-        runner.feed(normalize(model.meta.save_filename), Tensor.create(param_path.getBytes()));
-        runner.feed(normalize(model.meta.parameters.get("global_is_training")), Tensor.create(false));
-        runner.addTarget(normalize(model.meta.save_op));
-        runner.fetch(normalize(model.meta.save_op));
-        runner.run();
+        try(Tensor filenameTensor = Tensor.create(param_path.getBytes());
+            Tensor isTrainingTensor = Tensor.create(false)) {
+
+            feedIfPresent(runner, model.meta.save_filename, filenameTensor);
+            feedIfPresent(runner, model.meta.parameters.get("global_is_training"), isTrainingTensor);
+            runner.addTarget(normalize(model.meta.save_op));
+            runner.fetch(normalize(model.meta.save_op));
+            runner.run();
+        }
 
         File file = new File(param_path);
         File[] files = listFilesWithPrefix(file.getParentFile(), file.getName());
@@ -199,28 +215,30 @@ public class TensorflowBackend implements BackendTrain {
 
         FloatBuffer dataMatrix = model.createDataMatrix(data);
         long[] dataShape = {model.miniBatchSize, model.frameSize};
-        runner.feed(normalize(model.meta.inputs.get("batch_image_input")), Tensor.create(dataShape, dataMatrix));
-        runner.fetch(name);
 
-        Tensor run = runner.run().get(0);
+        try(Tensor dataTensor = Tensor.create(dataShape, dataMatrix)) {
+            runner.feed(normalize(model.meta.inputs.get("batch_image_input")), dataTensor);
+            runner.fetch(name);
 
-        long[] shape = run.shape();
-        int[] dims = new int[shape.length];
-        int flatten = 1;
-        for(int i = 0; i < shape.length; i++) {
-            long dim = shape[i];
-            dims[i] = (int) dim;
-            flatten *= dim;
+            Tensor run = runner.run().get(0);
+
+            long[] shape = run.shape();
+            int[] dims = new int[shape.length];
+            int flatten = 1;
+            for (int i = 0; i < shape.length; i++) {
+                long dim = shape[i];
+                dims[i] = (int) dim;
+                flatten *= dim;
+            }
+
+            Object[] original = (Object[]) Array.newInstance(float.class, dims);
+
+            run.copyTo(original);
+
+            float[] flattened = new float[flatten];
+            flatten(original, flattened, 0);
+            return flattened;
         }
-
-        Object[] original = (Object[]) Array.newInstance(float.class, dims);
-
-        run.copyTo(original);
-
-        float[] flattened = new float[flatten];
-        flatten(original, flattened, 0);
-
-        return flattened;
     }
 
     private int flatten(Object original, float[] flattened, int dstPos) {
@@ -343,40 +361,57 @@ public class TensorflowBackend implements BackendTrain {
 
         Session.Runner runner = model.getSession().runner();
 
-        runner.feed(
-                normalize(model.meta.inputs.get("batch_image_input")),
-                Tensor.create(dataShape, dataMatrix));
-        runner.feed(normalize(model.meta.inputs.get("categorical_labels")), Tensor.create(labelShape, labelData));
+        try(Tensor dataTensor = Tensor.create(dataShape, dataMatrix);
+            Tensor labelTensor = Tensor.create(labelShape, labelData);
+            Tensor batchSizeTensor = Tensor.create((float) model.miniBatchSize);
+            Tensor learningRateTensor = Tensor.create(model.getParameter("learning_rate", 0.001f));
+            Tensor momentumTensor = Tensor.create(model.getParameter("momentum", 0.9f));
+            Tensor isTrainingTensor = Tensor.create(true)) {
 
-        if(null != model.activations) {
-            feedMLPData(model, runner);
+            runner.feed(
+                    normalize(model.meta.inputs.get("batch_image_input")),
+                    dataTensor);
+            runner.feed(normalize(model.meta.inputs.get("categorical_labels")), labelTensor);
+
+            List<Tensor> mlpTensors = Collections.emptyList();
+            if (null != model.activations) {
+                mlpTensors = feedMLPData(model, runner);
+            }
+
+            feedIfPresent(runner, model.meta.parameters.get("batch_size"), batchSizeTensor);
+
+            feedIfPresent(runner, model.meta.parameters.get("learning_rate"), learningRateTensor);
+            feedIfPresent(runner, model.meta.parameters.get("momentum"), momentumTensor);
+
+            feedIfPresent(runner, model.meta.parameters.get("global_is_training"), isTrainingTensor);
+
+            runner.addTarget(normalize(model.meta.train_op));
+
+            runner.run();//nothing to fetch
+
+            for(Tensor t : mlpTensors) {
+                t.close();
+            }
+
+            return null;
         }
-
-        if(model.meta.parameters.containsKey("batch_size")) {
-            runner.feed(normalize(model.meta.parameters.get("batch_size")), Tensor.create((float) model.miniBatchSize));
-        }
-
-        runner.feed(normalize(model.meta.parameters.get("learning_rate")), Tensor.create(model.getParameter("learning_rate", 0.001f)));
-        runner.feed(normalize(model.meta.parameters.get("momentum")), Tensor.create(model.getParameter("momentum", 0.9f)));
-
-        runner.feed(normalize(model.meta.parameters.get("global_is_training")), Tensor.create(true));
-
-        runner.addTarget(normalize(model.meta.train_op));
-
-        runner.run();//nothing to fetch
-        return null;
     }
 
-    private void feedMLPData(TensorflowModel model, Session.Runner runner) {
+    private List<Tensor> feedMLPData(TensorflowModel model, Session.Runner runner) {
         // String tensors not supported in this version of TF Java API
         int[] act = new int[model.activations.length];
         for(int i = 0; i < model.activations.length; i++) {
             act[i] = TensorflowModel.activationToNumeric.getOrDefault(model.activations[i], 0);
         }
 
-        runner.feed(normalize(model.meta.parameters.get("activations")), Tensor.create(act));
-        runner.feed(normalize(model.meta.parameters.get("input_dropout")), Tensor.create(model.inputDropoutRatio));
-        runner.feed(normalize(model.meta.parameters.get("hidden_dropout")), Tensor.create(model.hiddenDropoutRatios));
+        Tensor activationTensor = Tensor.create(act);
+        runner.feed(normalize(model.meta.parameters.get("activations")), activationTensor);
+        Tensor dropoutRatioTensor = Tensor.create(model.inputDropoutRatio);
+        runner.feed(normalize(model.meta.parameters.get("input_dropout")), dropoutRatioTensor);
+        Tensor hiddenDropoutRatioTensor = Tensor.create(model.hiddenDropoutRatios);
+        runner.feed(normalize(model.meta.parameters.get("hidden_dropout")), hiddenDropoutRatioTensor);
+
+        return new ArrayList<Tensor>() {{this.add(activationTensor); this.add(dropoutRatioTensor); this.add(hiddenDropoutRatioTensor);}};
     }
 
     @Override
@@ -387,20 +422,29 @@ public class TensorflowBackend implements BackendTrain {
         FloatBuffer dataMatrix = model.createDataMatrix(data);
         long[] dataShape = {model.miniBatchSize, model.frameSize};
 
+        List<Tensor> mlpTensors = Collections.emptyList();
         if(null != model.activations) {
-            feedMLPData(model, runner);
+            mlpTensors = feedMLPData(model, runner);
         }
 
-        if(model.meta.parameters.containsKey("batch_size")){
-            runner.feed(normalize(model.meta.parameters.get("batch_size")), Tensor.create((float)model.miniBatchSize));
+        try(Tensor miniBatchSizeTensor = Tensor.create((float) model.miniBatchSize);
+            Tensor isTrainingTensor = Tensor.create(false);
+            Tensor dataTensor = Tensor.create(dataShape, dataMatrix)) {
+
+            feedIfPresent(runner, model.meta.parameters.get("batch_size"), miniBatchSizeTensor);
+
+            feedIfPresent(runner, model.meta.parameters.get("global_is_training"), isTrainingTensor);
+            feedIfPresent(runner, model.meta.inputs.get("batch_image_input"), dataTensor);
+            runner.fetch(normalize(model.meta.predict_op)); //the tensor we want to extract
+            List<Tensor> results = runner.run();
+
+            for(Tensor t : mlpTensors) {
+                t.close();
+            }
+
+            Tensor output = results.get(0);
+            return model.getPredictions(output);
         }
-        runner.feed(normalize(model.meta.parameters.get("global_is_training")), Tensor.create(false));
-        runner.feed(normalize(model.meta.inputs.get("batch_image_input")), Tensor.create(dataShape, dataMatrix));
-        runner.fetch(normalize(model.meta.predict_op)); //the tensor we want to extract
-        List<Tensor> results = runner.run();
-        Tensor output = results.get(0);
-        float[] preds = model.getPredictions(output);
-        return preds;
     }
 
 }
